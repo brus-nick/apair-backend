62c5cd1d71981fa3b685c8d7daa41b439588a506
Борис <boris-gavrishuk@mail.ru>
2016-06-07 19:36:28 +0300

    интерактивная таблица(журнал)

--- app/controllers/Hives.java
+++ app/controllers/Hives.java
@@ -8,9 +8,9 @@ import static play.mvc.Results.ok;
  * Created by Борис on 29.02.2016.
  */
 public class Hives {
-    public Result log(String a) {
+    public Result log(String temp, String hum) {
 
-        return ok(a);
+        return ok(temp, hum);
     }
 
 
