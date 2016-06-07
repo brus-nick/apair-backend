0f19ea3bf95f9777fa7d0b622ef774c82f6a63ac
Борис <boris-gavrishuk@mail.ru>
2016-06-08 00:34:30 +0300

    Тестовые данные

--- app/controllers/GetRecords.java
+++ app/controllers/GetRecords.java
@@ -18,8 +18,21 @@ public class GetRecords {
     {
         Form<Hives> hivesForm = Form.form(Hives.class).bindFromRequest();
         Hives hive = hivesForm.get();
+        hive = queryHives(hive);
+        Journal log = new Journal();
+
+        log.temp = 25;
+        log.hum = 28;
+
+        log.hive = hive;
+        hive.journals.add(log);
+        hive.update();
         List<Journal> journal = queryJournal(hive);
         return ok(toJson(journal));
+    }
+
+    public Hives queryHives(Hives hive){
+        return Hives.find.where().eq("hive_id", hive.hive_id).findUnique();
     }
 
     public List<Journal> queryJournal(Hives hive)
